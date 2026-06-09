fd15b1ce5141459521b722345ab56b1c74663364
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-06-09 16:47:53 +0530

    fix same problem

--- frontend/src/services/todoAPI.js
+++ frontend/src/services/todoAPI.js
@@ -1,17 +1,17 @@
 import api from "./api";
 
 export const getTodos = async () => {
-  return await api.get("/todo");
+  return await api.get("/api/v1/todo");
 };
 
 export const createTodo = async (data) => {
-  return await api.post("/todo/create", data);
+  return await api.post("/api/v1/todo/create", data);
 };
 
 export const updateTodo = async (id, data) => {
-  return await api.put(`/todo/${id}`, data);
+  return await api.put(`/api/v1/todo/${id}`, data);
 };
 
 export const deleteTodo = async (id) => {
-  return await api.delete(`/todo/${id}`);
+  return await api.delete(`/api/v1/todo/${id}`);
 };
