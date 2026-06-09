1aa353814c7273c1f066b3366b37dc888a26f9b8
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-06-09 16:37:40 +0530

    fix authAPI

--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -1,9 +1,9 @@
 import api from "./api";
 
 export const signupUser = async (data) => {
-  return await api.post("/user/signup", data);
+  return await api.post("/api/v1/user/signup", data);
 };
 
 export const loginUser = async (data) => {
-  return await api.post("/user/login", data);
+  return await api.post("/api/v1/user/login", data);
 };
